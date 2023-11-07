b6b0d7a623f84deb970dc88c3181f1ea794c951e
Sebastian Jensen <apps@tsjdev-apps.de>
2023-11-07 11:30:02 +0100

    Add model classes for requests and responses

---
+++ OpenAIDallE3Console/OpenAIDallE3Console/Models/GenerateImageResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OpenAIDallE3Console.Models;
+
+/// <summary>
+///     Represents a response from the 
+///     DALL-E 3 model for image generation.
+/// </summary>
+internal class GenerateImageResponse
+{
+    /// <summary>
+    ///     The URL of the generated image.
+    /// </summary>
+    [JsonPropertyName("url")]
+    public string? Url { get; set; } = null;
+
+    /// <summary>
+    ///     The revised prompt used for image generation.
+    /// </summary>
+    [JsonPropertyName("revised_prompt")]
+    public string? RevisedPrompt { get; set; } = null;
+}
---
+++ OpenAIDallE3Console/OpenAIDallE3Console/Models/GenerateImageRootResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OpenAIDallE3Console.Models;
+
+/// <summary>
+///     Represents the root object of a 
+///     response from the DALL-E 3 model 
+///     for image generation.
+/// </summary>
+internal class GenerateImageRootResponse
+{
+    /// <summary>
+    /// The timestamp of when the 
+    /// response was created.
+    /// </summary>
+    [JsonPropertyName("created")]
+    public long Created { get; set; }
+
+    /// <summary>
+    /// The array of generated images 
+    /// and their metadata.
+    /// </summary>
+    [JsonPropertyName("data")]
+    public GenerateImageResponse?[] Data { get; set; } = Array.Empty<GenerateImageResponse>();
+}
